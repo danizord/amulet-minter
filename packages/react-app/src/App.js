d315909eee9cbef62052c13a3528419980fd2ee4
Daniel Gimenes <daniel@danielgimenes.com.br>
2021-09-15 19:22:57 -0300

    Upgrade WalletButton
    Backporting https://github.com/paulrberg/create-eth-app/pull/146

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Contract } from "@ethersproject/contracts";
 import { Web3Provider } from "@ethersproject/providers";
 import { useQuery } from "@apollo/react-hooks";
@@ -56,6 +56,38 @@ async function mintAmulets() {
 }
 
 function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
+  const [account, setAccount] = useState("");
+  const [rendered, setRendered] = useState("");
+
+  useEffect(() => {
+    async function fetchAccount() {
+      try {
+        if (!provider) {
+          return;
+        }
+
+        // Load the user's accounts.
+        const accounts = await provider.listAccounts();
+        setAccount(accounts[0]);
+
+        // Resolve the ENS name for the first account.
+        const name = await provider.lookupAddress(accounts[0]);
+
+        // Render either the ENS name or the shortened account address.
+        if (name) {
+          setRendered(name);
+        } else {
+          setRendered(account.substring(0, 6) + "..." + account.substring(36));
+        }
+      } catch (err) {
+        setAccount("");
+        setRendered("");
+        console.error(err);
+      }
+    }
+    fetchAccount();
+  }, [account, provider, setAccount, setRendered]);
+
   return (
     <Button
       onClick={() => {
@@ -66,7 +98,8 @@ function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
         }
       }}
     >
-      {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+      {rendered === "" && "Connect Wallet"}
+      {rendered !== "" && rendered}
     </Button>
   );
 }
