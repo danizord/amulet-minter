1824c79d1a2e2178fca1fe51c08a1f7caad2e75a
Daniel Gimenes <daniel@danielgimenes.com.br>
2021-09-16 14:22:39 -0300

    Extract WalletButton component

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Contract } from "@ethersproject/contracts";
 import { Web3Provider } from "@ethersproject/providers";
-
+import { WalletButton } from "./components/WalletButton";
 import { Body, Button, Header } from "./components";
 import useWeb3Modal from "./hooks/useWeb3Modal";
 
@@ -31,55 +31,6 @@ async function mintAmulets() {
   );
 
   console.log({ res });
-}
-
-function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
-  const [account, setAccount] = useState("");
-  const [rendered, setRendered] = useState("");
-
-  useEffect(() => {
-    async function fetchAccount() {
-      try {
-        if (!provider) {
-          return;
-        }
-
-        // Load the user's accounts.
-        const accounts = await provider.listAccounts();
-        setAccount(accounts[0]);
-
-        // Resolve the ENS name for the first account.
-        const name = await provider.lookupAddress(accounts[0]);
-
-        // Render either the ENS name or the shortened account address.
-        if (name) {
-          setRendered(name);
-        } else {
-          setRendered(account.substring(0, 6) + "..." + account.substring(36));
-        }
-      } catch (err) {
-        setAccount("");
-        setRendered("");
-        console.error(err);
-      }
-    }
-    fetchAccount();
-  }, [account, provider, setAccount, setRendered]);
-
-  return (
-    <Button
-      onClick={() => {
-        if (!provider) {
-          loadWeb3Modal();
-        } else {
-          logoutOfWeb3Modal();
-        }
-      }}
-    >
-      {rendered === "" && "Connect Wallet"}
-      {rendered !== "" && rendered}
-    </Button>
-  );
 }
 
 function App() {
---
+++ packages/react-app/src/components/WalletButton.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { Button } from ".";
+
+export function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
+  const [account, setAccount] = useState("");
+  const [rendered, setRendered] = useState("");
+
+  useEffect(() => {
+    async function fetchAccount() {
+      try {
+        if (!provider) {
+          return;
+        }
+
+        // Load the user's accounts.
+        const accounts = await provider.listAccounts();
+        setAccount(accounts[0]);
+
+        // Resolve the ENS name for the first account.
+        const name = await provider.lookupAddress(accounts[0]);
+
+        // Render either the ENS name or the shortened account address.
+        if (name) {
+          setRendered(name);
+        } else {
+          setRendered(account.substring(0, 6) + "..." + account.substring(36));
+        }
+      } catch (err) {
+        setAccount("");
+        setRendered("");
+        console.error(err);
+      }
+    }
+    fetchAccount();
+  }, [account, provider, setAccount, setRendered]);
+
+  return (
+    <Button
+      onClick={() => {
+        if (!provider) {
+          loadWeb3Modal();
+        } else {
+          logoutOfWeb3Modal();
+        }
+      }}
+    >
+      {rendered === "" && "Connect Wallet"}
+      {rendered !== "" && rendered}
+    </Button>
+  );
+}
